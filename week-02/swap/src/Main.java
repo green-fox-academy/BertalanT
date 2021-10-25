fc9ff7f4a493256131f31f4afa26ad6e8022944e
BertalanT <eletmestere@gmail.com>
2021-10-25 18:32:20 +0200

    swap

---
+++ week-02/swap/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+  public static void main(String[] args) {
+    int a = 123;
+    int b = 526;
+    a = 526;
+    b = 123;
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
