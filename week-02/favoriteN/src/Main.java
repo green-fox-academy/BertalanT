1d104b854b468478b173fa6a9442242d40debc7e
BertalanT <eletmestere@gmail.com>
2021-10-25 18:26:37 +0200

    tasks

---
+++ week-02/favoriteN/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int favoriteNumber = 9;
+        System.out.println("A kedvenc számom: " + favoriteNumber);
+    }
+}
---
+++ week-02/twoNumbers/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println(13 + 22);
+    System.out.println(13 - 22);
+    System.out.println(13 * 22);
+  }
+}
