5b2c6ba1cf6f225296cc0b6e570ebd2cb3ca3467
BertalanT <eletmestere@gmail.com>
2021-10-25 16:55:12 +0200

    week2

---
+++ week-02/helloworld/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+//        ez egy egy soros comment
+//                kommentgyakorlás
+    /*több soros
+    comment blokk
+*/
+
+
+    }
+}
+
+
