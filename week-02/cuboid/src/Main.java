23ff77e6111239e7181bab962de39558cbf93939
BertalanT <eletmestere@gmail.com>
2021-10-25 19:08:37 +0200

    cuboid

---
+++ week-02/cuboid/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+  public static void main(String[] args) {
+    double aSide = 20;
+    double bSide = 30;
+    double cSide = 1.666666666666;
+    System.out.println("Felület: " + (aSide * bSide));
+    System.out.println("Térfogat: " + (aSide * bSide * cSide));
+  }
+}
