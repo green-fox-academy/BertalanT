6c75d2c6bcfdabcc840e72f29adfe2fd237450f0
BertalanT <eletmestere@gmail.com>
2021-10-25 17:21:28 +0200

    week-refresh

---
+++ week-02/data-of-me/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Tamas Bertalan");
+    System.out.println(43);
+    System.out.println(1.78);
+  }
+
+}
---
+++ week-02/lipem-lopom/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Lipem, lopom a szőlőt,");
+        System.out.println("elaludt az öreg csősz.");
+        System.out.println("Furkósbot a kezében,");
+        System.out.println("vaskalap a fejében.");
+    }
+}
