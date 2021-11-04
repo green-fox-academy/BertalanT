b310954846e75ae15bce0ff38b88c26112aa6613
BertalanT <eletmestere@gmail.com>
2021-11-04 17:41:38 +0100

    drawing2

---
+++ week-04/Horizontal/src/HorizontalLines2.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class HorizontalLines2 {
+  public static void mainDraw(Graphics graphics){
+    for (int i = 0; i < 3; i++) {
+      int x = getRandomXCoordinate();
+      int y = getRandomYCoordinate();
+      drawThree50LongHorizontalLine(x, y, graphics);
+    }
+
+
+  }
+  public static void drawThree50LongHorizontalLine (int x1,int y1, Graphics graphics){
+    int x2 = x1 + 50;
+    int y2 = y1;
+    graphics.drawLine(x1, y1, x2, y2);
+  }
+
+  public static int getRandomXCoordinate() {
+    Random random = new Random();
+    int limitX = WIDTH;
+    return random.nextInt(limitX);
+  }
+
+  public static int getRandomYCoordinate() {
+    Random random = new Random();
+    int limitX = HEIGHT;
+    return random.nextInt(limitX);
+  }
+
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
