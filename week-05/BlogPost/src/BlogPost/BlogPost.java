ce0b177bb614353ad154593d28969edfb584164e
BertalanT <eletmestere@gmail.com>
2021-11-10 16:23:31 +0100

    BlogPost

--- week-05/BlogPost/src/BlogPost/BlogPost.java
+++ week-05/BlogPost/src/BlogPost/BlogPost.java
@@ -1,4 +1,38 @@
 package BlogPost;
 
 public class BlogPost {
+  private String authorName;
+  private String title;
+  private String text;
+  private String publicationDate;
+
+  public BlogPost(String authorName, String title, String text, String publicationDate) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+  }
+
+  public String getAuthorName() {
+    return authorName;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public String getPublicationDate() {
+    return publicationDate;
+  }
 }
+
+
+//Készíts egy BlogPost osztályt, amely rendelkezik a következő tulajdonságokkal:
+//authorName (szerző neve)
+//title (cím)
+//text (szöveg)
+//publicationDate (kiadási dátum)
